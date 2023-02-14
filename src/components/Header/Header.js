f60991cc421a6488e8e4d88f7addd7a056f6f90b
Austin Chabaud <austinc1023@yahoo.com>
2023-02-14 14:42:58 -0600

    updated styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,61 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { DiJsBadge } from 'react-icons/di';
+import { DiReact } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+import {
+	Container,
+	Div1,
+	Div2,
+	Div3,
+	NavLink,
+	SocialIcons,
+	Span,
+} from './HeaderStyles';
 
-const Header = () =>  (
-  <Container>
-    <Div1>
-      <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px"}}>
-          <DiJsBadge size="3rem" style={{marginRight: "5px"}}/> <Span>Austin Chabaud</Span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href="#projects">
-          <NavLink>Projects</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#about">
-          <NavLink>About</NavLink>
-        </Link>
-      </li>
-    </Div2>
-    <Div3>
-      <SocialIcons href="https://github.com/austinchabaud">
-        <AiFillGithub size="3rem"/>
-      </SocialIcons>
-      <SocialIcons href="https://www.linkedin.com/in/austin-chabaud/">
-        <AiFillLinkedin size="3rem"/>
-      </SocialIcons>
-    </Div3>
-  </Container>
+const Header = () => (
+	<Container>
+		<Div1>
+			<Link href='/'>
+				<a
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						color: 'white',
+						marginBottom: '20px',
+					}}
+				>
+					<DiReact size='3rem' style={{ marginRight: '5px' }} />{' '}
+					<Span>Austin Chabaud</Span>
+				</a>
+			</Link>
+		</Div1>
+		<Div2>
+			<li>
+				<Link href='#projects'>
+					<NavLink>Projects</NavLink>
+				</Link>
+			</li>
+			<li>
+				<Link href='#tech'>
+					<NavLink>Technologies</NavLink>
+				</Link>
+			</li>
+			<li>
+				<Link href='#about'>
+					<NavLink>About</NavLink>
+				</Link>
+			</li>
+		</Div2>
+		<Div3>
+			<SocialIcons href='https://github.com/austinchabaud'>
+				<AiFillGithub size='3rem' />
+			</SocialIcons>
+			<SocialIcons href='https://www.linkedin.com/in/austin-chabaud/'>
+				<AiFillLinkedin size='3rem' />
+			</SocialIcons>
+		</Div3>
+	</Container>
 );
 
 export default Header;
