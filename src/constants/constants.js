73300fbf0d1770dfc94c14b0df9cfbbbd18337f1
Austin Chabaud <austinc1023@yahoo.com>
2023-02-14 15:36:36 -0600

    updated projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,30 +1,54 @@
 export const projects = [
-  {
-    title: 'Kindess Challenge',
-    description:"It is a community driven platform based on the crowdfunding information principle. It is built in React while incorporating the Google Maps api and Styled Components for styling. ",
-    image: '/images/kindness-img.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://daniellimhong.github.io/kindness-challenge',
-    visit: 'https://github.com/austinchabaud/kindness-challenge/',
-    id: 0,
-  },
-  {
-    title: 'GitHub Finder App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/githubfind.png',
-      tags: ['React', 'GitHub API'],
-    source: 'https://hardcore-borg-17bb74.netlify.app/',
-    visit: 'https://github.com/austinchabaud/githubsearch',
-    id: 1,
-  },
+	{
+		title: 'Kindess Challenge',
+		description:
+			'It is a community driven platform based on the crowdfunding information principle. It is built in React while incorporating the Google Maps api and Styled Components for styling. ',
+		image: '/images/kindness-img.png',
+		tags: ['React', 'JavaScript'],
+		source: 'https://daniellimhong.github.io/kindness-challenge',
+		visit: 'https://github.com/austinchabaud/kindness-challenge/',
+		id: 0,
+	},
+	{
+		title: 'GitHub Finder App',
+		description:
+			'This is a React application using the Github API to search for users in the Github database.',
+		image: '/images/githubfind.png',
+		tags: ['React', 'GitHub API'],
+		source: 'https://hardcore-borg-17bb74.netlify.app/',
+		visit: 'https://github.com/austinchabaud/githubsearch',
+		id: 1,
+	},
+	{
+		title: 'Hunting Season Countdown',
+		description:
+			'This is a countdown timer for the 2023-2024 Alabama Hunting Season using React.',
+		image: '/images/huntingcountdown.png',
+		tags: ['React', 'GitHub API'],
+		source: 'https://hardcore-borg-17bb74.netlify.app/',
+		visit: 'https://github.com/austinchabaud/githubsearch',
+		id: 2,
+	},
 ];
 
 export const TimeLineData = [
-  { year: 2016, text: 'Began studying HTML, CSS, and JavaScript.', },
-  { year: 2017, text: 'Found various internships and contract positions to strengthen my skills.', },
-  { year: 2018, text: 'Built and maintained various web applications.', },
-  { year: 2019, text: 'Attended DevMountain 13 week web development bootcamp.', },
-  { year: 2020, text: 'Architected React projects of my own. Took a deep dive into UI/UX development.', },
-  { year: 2021, text: 'Worked for Hexagon as a Software Developer.', },
-  { year: 2022, text: 'Constantly improving my skills and now working as a Software Engineer I for Vertafore!', },
+	{ year: 2016, text: 'Began studying HTML, CSS, and JavaScript.' },
+	{
+		year: 2017,
+		text: 'Found various internships and contract positions to strengthen my skills.',
+	},
+	{ year: 2018, text: 'Built and maintained various web applications.' },
+	{
+		year: 2019,
+		text: 'Attended DevMountain 13 week web development bootcamp.',
+	},
+	{
+		year: 2020,
+		text: 'Architected React projects of my own. Took a deep dive into UI/UX development.',
+	},
+	{ year: 2021, text: 'Worked for Hexagon as a Software Developer.' },
+	{
+		year: 2022,
+		text: 'Constantly improving my skills and now working as a Software Engineer I for Vertafore!',
+	},
 ];
